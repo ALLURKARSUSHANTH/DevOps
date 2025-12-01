871e9045257998866767735f8e9fcba541be4670
ALLURKARSUSHANTH <sushanth9524@gmail.com>
2025-12-01 18:19:00 +0530

    update

---
+++ DevOps/server.js
@@ -0,0 +1,9 @@
+const https = require("https");
+const port = 3000;
+
+const server = https.createServer((req,res)=>{
+    res.writeHead(200,{'content-type':'text/plain'})
+    res.send(`<h2> Hello,I'm Practicing </h2> \n`);
+});
+
+server.listen(port,()=> console.log(`server running on port ${port}`));
