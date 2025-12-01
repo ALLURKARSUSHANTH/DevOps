d31138298d32d5db0585eb9750a729e013b61a30
Sushanth <146927710+ALLURKARSUSHANTH@users.noreply.github.com>
2025-12-01 13:34:07 +0000

    update

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const http = require("http");
 const port = 8082;
 
-const server = http.createServer((req,res)=>{
-    res.writeHead(200,{'content-type':'text/plain'})
-    res.send(`<h2> Hello,I'm Practicing </h2> \n`);
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.end(`<h2>Hello, I'm Practicing</h2>\n`);
 });
 
-server.listen(port,()=> console.log(`server running on port ${port}`));
+server.listen(port, '0.0.0.0', () => console.log(`Server running on port ${port}`));
